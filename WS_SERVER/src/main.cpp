6deb122c892b30bba350625da94f2884e40abef1
zbl2018 <191904554@qq.com>
2018-04-19 14:35:17 +0800

    整合了web、client测试demo,重写了makefile文件，解决了一些已知的bug

---
+++ WS_SERVER/src/main.cpp
@@ -0,0 +1,11 @@
+#include"../include/ws_server.h"
+int main()
+{
+    ws_server WS;
+    WS.InitTcpClient_ConnectServer("127.0.0.1",22222);
+    WS.init_ws_server();
+    //Listen on port 44444
+    WS.ws_run(20000);
+
+    return 0;
+}
